4b64e152c7934c9567f8a0e69e317787cc5e058c
udaypydi <udaypydi333@gmail.com>
2020-06-08 11:15:09 +0530

    feat(hooks): add loading state to use fetch

--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -1,18 +1,21 @@
 function useFetch(url, options = {}) {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
   
     useEffect(() => {
       setIsLoading(true);
       fetch(url, options)
         .then(res => res.json())
         .then(json => {
+          setIsLoading(false);
           setData(json);
         })
         .catch(err => {
+          setIsLoading(false);
           setError(err);
         });
     }, []);
   
-    return { data, error };
+    return { data, error, isLoading };
   }
