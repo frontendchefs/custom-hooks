215078d91f7ac0d9be6de795c056e1ac968d88eb
udaypydi <udaypydi333@gmail.com>
2020-06-08 11:13:23 +0530

    feat(hooks): add error states in use fetch hook

--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -1,16 +1,18 @@
 function useFetch(url, options = {}) {
     const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
   
     useEffect(() => {
+      setIsLoading(true);
       fetch(url, options)
         .then(res => res.json())
         .then(json => {
           setData(json);
         })
         .catch(err => {
-          return err;
+          setError(err);
         });
     }, []);
   
-    return data;
+    return { data, error };
   }
