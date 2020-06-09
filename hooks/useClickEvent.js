272b1c4d9d08af2af39b5ffedef0edcba18dea0b
udaypydi <udaypydi333@gmail.com>
2020-06-09 23:42:06 +0530

    feat(hooks): add support for element and id in use click event

--- hooks/useClickEvent.js
+++ hooks/useClickEvent.js
@@ -1,10 +1,16 @@
-function useClickEvent(targetElement, fn) {
-    useEffect(() => {
-      targetElement.addEventListener("click", fn);
-  
-      return () => {
-        targetElement.removeEventListener("click", fn);
-      };
-    }, []);
+function useClickEvent({ element, id }, fn) {
+  let targetElement;
+  if (element) {
+    targetElement = element;
   }
+  if (id) {
+    targetElement = document.getElementById(id);
+  }
+  useEffect(() => {
+    targetElement.addEventListener("click", fn);
+    return () => {
+      targetElement.removeEventListener("click", fn);
+    };
+  }, []);
+}
   
