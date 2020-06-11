e2438bba7cf5fac59c8c17863c9371de9d0b25c6
udaypydi <udaypydi333@gmail.com>
2020-06-11 23:51:08 +0530

    feat(hook): use local storage set item added

--- hooks/useLocalStorage.js
+++ hooks/useLocalStorage.js
@@ -1,10 +1,13 @@
-function useLocalStorage(key, operationType) {
+function useLocalStorage({ key, value }, operationType) {
     try {
         if (operationType === 'GET') {
             const data = window.localStorage.getItem(key);
             return { data: JSON.parse(data), status: 'SUCCESS' };
+        } else if (operationType === 'SET') {
+            window.localStorage.setItem(key, JSON.stringify(value));
+            return { status: 'SUCCESS' };
         }
-    } catch(err) {
+     } catch(err) {
         return { data: err, status: 'ERROR' };
     }
     
